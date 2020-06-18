8dc3969a7a13b9fcebc33cd578a2c9756a7f87d5
Samuel Odudare <odudaresamuel@yahoo.co.uk>
2020-06-18 15:53:48 +0100

    created enderTotalIncome and renderMostValuablePayment

--- app.js
+++ app.js
@@ -36,7 +36,7 @@
 var account = {
   number: 100402153,
   initialBalance: 100,
-  paymentsUrl: '/data/payments.json',
+  paymentsUrl: "/data/payments.json",
   payments: []
 };
 
@@ -48,15 +48,14 @@ var account = {
  *
  * You may edit this code.
  */
-document.querySelector('#loadButton')
-  .addEventListener('click', function () {
-    fetch(account.paymentsUrl)
-      .then(response => response.json())
-      .then(payments => {
-        account.payments = payments;
-        render(account);
-      });
-  });
+document.querySelector("#loadButton").addEventListener("click", function() {
+  fetch(account.paymentsUrl)
+    .then(response => response.json())
+    .then(payments => {
+      account.payments = payments;
+      render(account);
+    });
+});
 
 /**
  * Write a render function below that updates the DOM with the
@@ -72,11 +71,9 @@ document.querySelector('#loadButton')
  * @param {Object} account The account details
  */
 function render(account) {
-
   // Display the account number
-  document.querySelector('#accountNumber')
-    .innerText = account.number;
-};
+  document.querySelector("#accountNumber").innerText = account.number;
+}
 
 /**
  * Write any additional functions that you need to complete
@@ -86,3 +83,35 @@ function render(account) {
  * calculate balances, find completed or pending payments,
  * add up payments, and more.
  */
+
+//No 4: Total income of all payments that were received this month (May, 2019), including pending payments.
+
+function renderTotalIncome(account) {
+  let totalIncomePayments = account.payments
+    .filter(mayDated)
+    .map(payment => payment.amount)
+    .reduce((accumulator, currentValue) => accumulator + currentValue);
+  function mayDated(payment) {
+    let mayDated = payment.date.split("-");
+    return mayDated[1] == "05";
+  }
+  document.querySelector("#totalIncome").textContent = `£${totalIncome}`;
+}
+
+//No 5: Show the amount of the most valuable payment that was received this month (May 2019.
+
+function renderMostValuablePayment(account) {
+  let mostValuablePayment = account.payments
+    .filter(mayDated)
+    .map(payment => payment.amount)
+    .reduce(function(a, b) {
+      return Math.max(a, b);
+    });
+  function mayDated(payment) {
+    let mayDated = payment.date.split("-");
+    return mayDated[1] == "05" && payment.completed == true;
+  }
+  document.querySelector(
+    "#mostValuablePayment"
+  ).textContent = `£${mostValuablePayment}`;
+}
